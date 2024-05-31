8030678f89616f459903f0717e29a02dbf79319e
Yifan Wang <35350620+WevenWang@users.noreply.github.com>
2024-05-31 10:05:14 -0700

    move context to layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
 "use client";
-import Image from "next/image";
-import styles from "./page.module.css";
-import {
-	Box,
-	Button,
-	Card,
-	CardHeader,
-	Divider,
-	Stack,
-	styled,
-	TextField,
-	Typography,
-} from "@mui/material";
+import { Stack } from "@mui/material";
 import { TaskListContextProvider } from "./context/TaskListContext";
 import Board from "./components/Board";
 import SideNav from "./components/SideNav";
@@ -20,21 +8,19 @@ import useResponsive from "./hooks/useResponsive";
 export default function Home() {
 	const mdUp = useResponsive("up", "md");
 	return (
-		<TaskListContextProvider>
-			<Stack
-				sx={{
-					width: "100%",
-					minHeight: "100vh",
+		<Stack
+			sx={{
+				width: "100%",
+				minHeight: "100vh",
 
-					bgcolor: "background.default",
-				}}
-				direction="row"
-				spacing={2}
-			>
-				{mdUp && <SideNav />}
+				bgcolor: "background.default",
+			}}
+			direction="row"
+			spacing={2}
+		>
+			{mdUp && <SideNav />}
 
-				<Board />
-			</Stack>
-		</TaskListContextProvider>
+			<Board />
+		</Stack>
 	);
 }
